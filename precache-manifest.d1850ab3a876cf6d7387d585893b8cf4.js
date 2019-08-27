b5e6b6df5efa830ac27d88df7a51ea37c9f94a30
basedvegan <pojomanda@gmail.com>
2019-08-27 14:09:49 -0700

    Updates

---
+++ precache-manifest.d1850ab3a876cf6d7387d585893b8cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cee90f0db4eb173b726321148e4680",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56a8e8af4cec87a1500",
+    "url": "/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c56a8e8af4cec87a1500",
+    "url": "/static/js/main.04a9b8e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
